4f3f732e09c5d90af54221f42ae4ebb42ab3be95
RichardsonRayshorn <rayshorndc@gmail.com>
2019-11-28 05:13:55 -0400

    updating to try and fix loading issue

--- server.js
+++ server.js
@@ -23,27 +23,27 @@ app.get('/listJobs', (req, res) =>
     getJobs((error, result) => 
     {
         console.log("test");
-        if(error)
-        {
-            console.log(error);
-        }
-        console.log(JSON.stringify(result));
+        // if(error)
+        // {
+        //     console.log(error);
+        // }
+        // console.log(JSON.stringify(result));
     });
     res.end();
 });
 
 function getJobs(callback) 
 {
-    var sql = "SELECT title, description, salary FROM jobs";
+    // var sql = "SELECT title, description, salary FROM jobs";
 
-    pool.query(sql, function(err, result) 
-    {
-        if(err)
-        {
-            console.log(err);
-        }
-        callback(null, result.rows);
-    })
+    // pool.query(sql, function(err, result) 
+    // {
+    //     if(err)
+    //     {
+    //         console.log(err);
+    //     }
+    //     callback(null, result.rows);
+    // })
 }
 
 app.listen(port);
