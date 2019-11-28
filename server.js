1469d0ae207e847ffe0aabac9ad31f6c44f5478d
RichardsonRayshorn <rayshorndc@gmail.com>
2019-11-28 04:26:09 -0400

    updating to try and fix loading issue

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ var express = require("express");
 var app = express();
 var port = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => 
+{
+    res.render(index);
+    res.end();
+});
+
+app.get('/listJobs', (req, res) => 
 {
     res.write("Testing");
     res.end();
