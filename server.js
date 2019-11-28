3cb7829321c11104f96e600d5eee6bcafd0ec401
RichardsonRayshorn <rayshorndc@gmail.com>
2019-11-28 05:07:20 -0400

    updating to try and fix loading issue

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 var express = require("express");
 var app = express();
+
+const { Pool } = require("pg");
+
+const connectionString = process.env.DATABASE_URL || "postgres://username:password@locationofserver:port/nameofdatabasetoconnectto";
+const pool = new Pool({connectionString: connectionString});
+
 var port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
@@ -13,8 +19,31 @@ app.get('/', (req, res) =>
 app.get('/listJobs', (req, res) => 
 {
     res.write("Testing");
+
+    getJobs((error, result) => 
+    {
+        if(error)
+        {
+            console.log(error);
+        }
+        console.log(JSON.stringify(result))
+    });
     res.end();
 });
+
+function getJobs(callback) 
+{
+    var sql = "SELECT title, description, salary FROM jobs";
+
+    pool.query(sql, function(err, result) 
+    {
+        if(err)
+        {
+            console.log(err);
+        }
+        callback(null, result.rows);
+    })
+}
 
 app.listen(port);
 console.log("testing the server");
