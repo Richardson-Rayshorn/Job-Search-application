7c56170440b63c83797ee0302e3df6660ff49931
RichardsonRayshorn <rayshorndc@gmail.com>
2019-12-01 00:29:17 -0400

    adding new endpoint service

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require("express");
+var bodyParser = require('body-parser');
 var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const { Pool } = require("pg");
 
