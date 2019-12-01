b010db9b43e8e21622a5709c5b8f18f04f1d4cbc
RichardsonRayshorn <rayshorndc@gmail.com>
2019-11-30 23:07:25 -0400

    adding new endpoint service

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -13,3 +13,19 @@ function listJobs()
     xmlhttp.open("GET","/listJobs",true);
     xmlhttp.send();
 }
+
+function postJobs()
+{
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = () => 
+    {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200)
+        {
+            document.getElementById('displayJobs').innerHTML=xmlhttp.responseText;
+        }
+    }
+
+    xmlhttp.open("GET","/postJobs",true);
+    xmlhttp.send();
+}
